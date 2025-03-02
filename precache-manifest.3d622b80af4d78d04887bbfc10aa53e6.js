93d6fb5931d92b7b2378704eca388ed5480dc21b
Ludovico Scarton <ludovico.scarton@gmail.com>
2025-03-01 21:24:01 -0500

    Updates

---
+++ precache-manifest.3d622b80af4d78d04887bbfc10aa53e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ceb0042417d7cb5a8b094bf1b762dee",
+    "url": "/german-adjective-game/index.html"
+  },
+  {
+    "revision": "1eb0e8ef17eb5ada862a",
+    "url": "/german-adjective-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/german-adjective-game/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/german-adjective-game/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "1eb0e8ef17eb5ada862a",
+    "url": "/german-adjective-game/static/js/main.e5a5403e.chunk.js"
+  },
+  {
+    "revision": "788635ad2a8cd901d984",
+    "url": "/german-adjective-game/static/js/runtime~main.1b3ce85e.js"
+  }
+]);
